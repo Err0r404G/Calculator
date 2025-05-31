a6c5990676a1654c16f81346a4c4da83c431105c
ErrOr404G <saib09837@gmail.com>
2025-05-31 19:47:19 +0600

    Completed Code

--- Calculator/Calculator.Designer.cs
+++ Calculator/Calculator.Designer.cs
@@ -53,14 +53,16 @@
             // 
             // inputBox
             // 
+            inputBox.BackColor = SystemColors.Info;
             inputBox.BorderStyle = BorderStyle.FixedSingle;
             inputBox.Cursor = Cursors.IBeam;
             inputBox.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             inputBox.Location = new Point(12, 72);
             inputBox.Name = "inputBox";
-            inputBox.RightToLeft = RightToLeft.Yes;
+            inputBox.RightToLeft = RightToLeft.No;
             inputBox.Size = new Size(342, 47);
             inputBox.TabIndex = 0;
+            inputBox.TextAlign = HorizontalAlignment.Right;
             // 
             // acButton
             // 
@@ -68,12 +70,13 @@
             acButton.Cursor = Cursors.Hand;
             acButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             acButton.ForeColor = SystemColors.Control;
-            acButton.Location = new Point(12, 153);
+            acButton.Location = new Point(18, 153);
             acButton.Name = "acButton";
             acButton.Size = new Size(155, 66);
             acButton.TabIndex = 1;
             acButton.Text = "AC";
             acButton.UseVisualStyleBackColor = false;
+            acButton.Click += acButton_Click;
             // 
             // percentButton
             // 
@@ -81,12 +84,13 @@
             percentButton.Cursor = Cursors.Hand;
             percentButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             percentButton.ForeColor = SystemColors.Control;
-            percentButton.Location = new Point(187, 153);
+            percentButton.Location = new Point(193, 153);
             percentButton.Name = "percentButton";
             percentButton.Size = new Size(69, 66);
             percentButton.TabIndex = 2;
             percentButton.Text = "%";
             percentButton.UseVisualStyleBackColor = false;
+            percentButton.Click += percentButton_Click;
             // 
             // divideButton
             // 
@@ -94,12 +98,13 @@
             divideButton.Cursor = Cursors.Hand;
             divideButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             divideButton.ForeColor = SystemColors.Control;
-            divideButton.Location = new Point(275, 153);
+            divideButton.Location = new Point(281, 153);
             divideButton.Name = "divideButton";
             divideButton.Size = new Size(67, 66);
             divideButton.TabIndex = 3;
             divideButton.Text = "/";
             divideButton.UseVisualStyleBackColor = false;
+            divideButton.Click += divideButton_Click;
             // 
             // nineButton
             // 
@@ -107,12 +112,13 @@
             nineButton.Cursor = Cursors.Hand;
             nineButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             nineButton.ForeColor = SystemColors.Control;
-            nineButton.Location = new Point(189, 239);
+            nineButton.Location = new Point(195, 239);
             nineButton.Name = "nineButton";
             nineButton.Size = new Size(67, 66);
             nineButton.TabIndex = 5;
             nineButton.Text = "9";
             nineButton.UseVisualStyleBackColor = false;
+            nineButton.Click += nineButton_Click;
             // 
             // sevenButton
             // 
@@ -120,12 +126,13 @@
             sevenButton.Cursor = Cursors.Hand;
             sevenButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             sevenButton.ForeColor = SystemColors.Control;
-            sevenButton.Location = new Point(12, 239);
+            sevenButton.Location = new Point(18, 239);
             sevenButton.Name = "sevenButton";
             sevenButton.Size = new Size(69, 66);
             sevenButton.TabIndex = 4;
             sevenButton.Text = "7";
             sevenButton.UseVisualStyleBackColor = false;
+            sevenButton.Click += sevenButton_Click;
             // 
             // multiplyButton
             // 
@@ -133,12 +140,13 @@
             multiplyButton.Cursor = Cursors.Hand;
             multiplyButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             multiplyButton.ForeColor = SystemColors.Control;
-            multiplyButton.Location = new Point(275, 239);
+            multiplyButton.Location = new Point(281, 239);
             multiplyButton.Name = "multiplyButton";
             multiplyButton.Size = new Size(67, 66);
             multiplyButton.TabIndex = 7;
             multiplyButton.Text = "*";
             multiplyButton.UseVisualStyleBackColor = false;
+            multiplyButton.Click += multiplyButton_Click;
             // 
             // eightButton
             // 
@@ -146,12 +154,13 @@
             eightButton.Cursor = Cursors.Hand;
             eightButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             eightButton.ForeColor = SystemColors.Control;
-            eightButton.Location = new Point(100, 239);
+            eightButton.Location = new Point(106, 239);
             eightButton.Name = "eightButton";
             eightButton.Size = new Size(69, 66);
             eightButton.TabIndex = 6;
             eightButton.Text = "8";
             eightButton.UseVisualStyleBackColor = false;
+            eightButton.Click += eightButton_Click;
             // 
             // subButton
             // 
@@ -159,12 +168,13 @@
             subButton.Cursor = Cursors.Hand;
             subButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             subButton.ForeColor = SystemColors.Control;
-            subButton.Location = new Point(275, 327);
+            subButton.Location = new Point(281, 327);
             subButton.Name = "subButton";
             subButton.Size = new Size(67, 66);
             subButton.TabIndex = 11;
             subButton.Text = "-";
             subButton.UseVisualStyleBackColor = false;
+            subButton.Click += subButton_Click;
             // 
             // sixButton
             // 
@@ -172,12 +182,13 @@
             sixButton.Cursor = Cursors.Hand;
             sixButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             sixButton.ForeColor = SystemColors.Control;
-            sixButton.Location = new Point(187, 327);
+            sixButton.Location = new Point(193, 327);
             sixButton.Name = "sixButton";
             sixButton.Size = new Size(69, 66);
             sixButton.TabIndex = 10;
             sixButton.Text = "6";
             sixButton.UseVisualStyleBackColor = false;
+            sixButton.Click += sixButton_Click;
             // 
             // fourButton
             // 
@@ -185,12 +196,13 @@
             fourButton.Cursor = Cursors.Hand;
             fourButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             fourButton.ForeColor = SystemColors.Control;
-            fourButton.Location = new Point(14, 327);
+            fourButton.Location = new Point(20, 327);
             fourButton.Name = "fourButton";
             fourButton.Size = new Size(67, 66);
             fourButton.TabIndex = 9;
             fourButton.Text = "4";
             fourButton.UseVisualStyleBackColor = false;
+            fourButton.Click += fourButton_Click;
             // 
             // fiveButton
             // 
@@ -198,12 +210,13 @@
             fiveButton.Cursor = Cursors.Hand;
             fiveButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             fiveButton.ForeColor = SystemColors.Control;
-            fiveButton.Location = new Point(100, 327);
+            fiveButton.Location = new Point(106, 327);
             fiveButton.Name = "fiveButton";
             fiveButton.Size = new Size(69, 66);
             fiveButton.TabIndex = 8;
             fiveButton.Text = "5";
             fiveButton.UseVisualStyleBackColor = false;
+            fiveButton.Click += fiveButton_Click;
             // 
             // equalButton
             // 
@@ -211,12 +224,13 @@
             equalButton.Cursor = Cursors.Hand;
             equalButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             equalButton.ForeColor = SystemColors.Control;
-            equalButton.Location = new Point(275, 502);
+            equalButton.Location = new Point(281, 502);
             equalButton.Name = "equalButton";
             equalButton.Size = new Size(67, 66);
             equalButton.TabIndex = 19;
             equalButton.Text = "=";
             equalButton.UseVisualStyleBackColor = false;
+            equalButton.Click += equalButton_Click;
             // 
             // oneButton
             // 
@@ -224,12 +238,13 @@
             oneButton.Cursor = Cursors.Hand;
             oneButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             oneButton.ForeColor = SystemColors.Control;
-            oneButton.Location = new Point(14, 414);
+            oneButton.Location = new Point(20, 414);
             oneButton.Name = "oneButton";
             oneButton.Size = new Size(69, 66);
             oneButton.TabIndex = 18;
             oneButton.Text = "1";
             oneButton.UseVisualStyleBackColor = false;
+            oneButton.Click += oneButton_Click;
             // 
             // twoButton
             // 
@@ -237,12 +252,13 @@
             twoButton.Cursor = Cursors.Hand;
             twoButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             twoButton.ForeColor = SystemColors.Control;
-            twoButton.Location = new Point(102, 414);
+            twoButton.Location = new Point(108, 414);
             twoButton.Name = "twoButton";
             twoButton.Size = new Size(67, 66);
             twoButton.TabIndex = 17;
             twoButton.Text = "2";
             twoButton.UseVisualStyleBackColor = false;
+            twoButton.Click += twoButton_Click;
             // 
             // threeButton
             // 
@@ -250,12 +266,13 @@
             threeButton.Cursor = Cursors.Hand;
             threeButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             threeButton.ForeColor = SystemColors.Control;
-            threeButton.Location = new Point(189, 414);
+            threeButton.Location = new Point(195, 414);
             threeButton.Name = "threeButton";
             threeButton.Size = new Size(69, 66);
             threeButton.TabIndex = 16;
             threeButton.Text = "3";
             threeButton.UseVisualStyleBackColor = false;
+            threeButton.Click += threeButton_Click;
             // 
             // sumButton
             // 
@@ -263,12 +280,13 @@
             sumButton.Cursor = Cursors.Hand;
             sumButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             sumButton.ForeColor = SystemColors.Control;
-            sumButton.Location = new Point(275, 414);
+            sumButton.Location = new Point(281, 414);
             sumButton.Name = "sumButton";
             sumButton.Size = new Size(67, 66);
             sumButton.TabIndex = 15;
             sumButton.Text = "+";
             sumButton.UseVisualStyleBackColor = false;
+            sumButton.Click += sumButton_Click;
             // 
             // zerozeroButton
             // 
@@ -276,12 +294,13 @@
             zerozeroButton.Cursor = Cursors.Hand;
             zerozeroButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             zerozeroButton.ForeColor = SystemColors.Control;
-            zerozeroButton.Location = new Point(100, 502);
+            zerozeroButton.Location = new Point(106, 502);
             zerozeroButton.Name = "zerozeroButton";
             zerozeroButton.Size = new Size(69, 66);
             zerozeroButton.TabIndex = 14;
             zerozeroButton.Text = "00";
             zerozeroButton.UseVisualStyleBackColor = false;
+            zerozeroButton.Click += zerozeroButton_Click;
             // 
             // zeroButton
             // 
@@ -289,12 +308,13 @@
             zeroButton.Cursor = Cursors.Hand;
             zeroButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             zeroButton.ForeColor = SystemColors.Control;
-            zeroButton.Location = new Point(14, 502);
+            zeroButton.Location = new Point(20, 502);
             zeroButton.Name = "zeroButton";
             zeroButton.Size = new Size(67, 66);
             zeroButton.TabIndex = 13;
             zeroButton.Text = "0";
             zeroButton.UseVisualStyleBackColor = false;
+            zeroButton.Click += zeroButton_Click;
             // 
             // dotButton
             // 
@@ -302,23 +322,28 @@
             dotButton.Cursor = Cursors.Hand;
             dotButton.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             dotButton.ForeColor = SystemColors.Control;
-            dotButton.Location = new Point(187, 502);
+            dotButton.Location = new Point(193, 502);
             dotButton.Name = "dotButton";
             dotButton.Size = new Size(69, 66);
             dotButton.TabIndex = 12;
             dotButton.Text = ".";
             dotButton.UseVisualStyleBackColor = false;
+            dotButton.Click += dotButton_Click;
             // 
             // previousBox
             // 
+            previousBox.BackColor = SystemColors.Info;
             previousBox.BorderStyle = BorderStyle.FixedSingle;
-            previousBox.Cursor = Cursors.IBeam;
+            previousBox.Cursor = Cursors.Hand;
             previousBox.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            previousBox.Location = new Point(14, 19);
+            previousBox.ImeMode = ImeMode.Off;
+            previousBox.Location = new Point(12, 19);
             previousBox.Name = "previousBox";
+            previousBox.ReadOnly = true;
             previousBox.RightToLeft = RightToLeft.Yes;
             previousBox.Size = new Size(342, 30);
             previousBox.TabIndex = 20;
+            previousBox.Click += previousBox_Click;
             // 
             // Calculator
             // 
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -16,5 +16,212 @@ namespace Calculator
         {
             InitializeComponent();
         }
+        private bool IsOperator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+        private void acButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text = "";
+        }
+
+        private void percentButton_Click(object sender, EventArgs e)
+        {
+            string input = inputBox.Text;
+
+            int lastOperatorIndex = input.LastIndexOfAny(new char[] { '+', '-', '*', '/' });
+
+            string lastNumber;
+            string beforeLastNumber;
+
+            if (lastOperatorIndex == -1)
+            {
+                lastNumber = input;
+                beforeLastNumber = "";
+            }
+            else
+            {
+                lastNumber = input.Substring(lastOperatorIndex + 1);
+                beforeLastNumber = input.Substring(0, lastOperatorIndex + 1);
+            }
+
+            if (double.TryParse(lastNumber, out double number))
+            {
+                double percentValue = number / 100.0;
+                inputBox.Text = beforeLastNumber + percentValue.ToString();
+            }
+            else
+            {
+            }
+        }
+
+
+        private void sevenButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "7";
+        }
+
+        private void eightButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "8";
+        }
+
+        private void nineButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "9";
+        }
+
+
+
+        private void fourButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "4";
+        }
+
+        private void fiveButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "5";
+        }
+
+        private void sixButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "6";
+        }
+
+
+        private void oneButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "1";
+        }
+
+        private void twoButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "2";
+        }
+
+        private void threeButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "3";
+        }
+
+
+        private void zeroButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "0";
+        }
+
+        private void zerozeroButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text += "00";
+        }
+        //Sum button event handler
+        private void sumButton_Click(object sender, EventArgs e)
+        {
+            if (inputBox.Text.Length == 0)
+                return;
+
+            char lastChar = inputBox.Text[inputBox.Text.Length - 1];
+            if (!IsOperator(lastChar))
+            {
+                inputBox.Text += "+";
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text.Substring(0, inputBox.Text.Length - 1) + "+";
+            }
+        }
+        //Sub button event handler
+        private void subButton_Click(object sender, EventArgs e)
+        {
+            if (inputBox.Text.Length == 0)
+                return;
+
+            char lastChar = inputBox.Text[inputBox.Text.Length - 1];
+            if (!IsOperator(lastChar))
+            {
+                inputBox.Text += "-";
+            }
+            else
+            {
+
+                inputBox.Text = inputBox.Text.Substring(0, inputBox.Text.Length - 1) + "-";
+            }
+        }
+
+
+        //Multiply button event handler
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            if (inputBox.Text.Length == 0)
+                return;
+
+            char lastChar = inputBox.Text[inputBox.Text.Length - 1];
+            if (!IsOperator(lastChar))
+            {
+                inputBox.Text += "*";
+            }
+            else
+            {
+
+                inputBox.Text = inputBox.Text.Substring(0, inputBox.Text.Length - 1) + "*";
+            }
+        }
+
+        //Divide button event handler
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            if (inputBox.Text.Length == 0)
+                return;
+
+            char lastChar = inputBox.Text[inputBox.Text.Length - 1];
+            if (!IsOperator(lastChar))
+            {
+                inputBox.Text += "/";
+            }
+            else
+            {
+                inputBox.Text = inputBox.Text.Substring(0, inputBox.Text.Length - 1) + "/";
+            }
+        }
+        //dot button event handler
+        private void dotButton_Click(object sender, EventArgs e)
+        {
+            int lastOperatorIndex = inputBox.Text.LastIndexOfAny(new char[] { '+', '-', '*', '/' });
+
+            string lastNumber;
+            if (lastOperatorIndex == -1)
+                lastNumber = inputBox.Text;
+            else
+                lastNumber = inputBox.Text.Substring(lastOperatorIndex + 1);
+
+            if (!lastNumber.Contains("."))
+            {
+                inputBox.Text += ".";
+            }
+        }
+        //Equal button event handler
+        private void equalButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string expression = inputBox.Text;
+                DataTable dt = new DataTable();
+                var result = dt.Compute(expression, "");
+
+                double numericResult = Convert.ToDouble(result);
+                inputBox.Text = numericResult.ToString("F2");
+                previousBox.Text = expression;
+
+            }
+            catch (Exception)
+            {
+                inputBox.Text = "Error";
+            }
+        }
+
+        private void previousBox_Click(object sender, EventArgs e)
+        {
+            inputBox.Text = previousBox.Text;
+        }
     }
 }
